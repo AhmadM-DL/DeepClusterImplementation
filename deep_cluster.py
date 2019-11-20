e5d0ab1ccc4e437f3c1acc235aa624470f92d09d
amm90 <amm90@mail.aub.edu>
2019-11-20 13:52:55 +0200

    Quick _ fixed __preprocess_neural_features

--- deep_cluster.py
+++ deep_cluster.py
@@ -15,7 +15,6 @@ from sklearn.cluster import KMeans
 
 
 class labels_reassigned_dataset(data.Dataset):
-    
     """A dataset where the new images labels are given in argument.
     Args:
         image_indexes (list): list of data indexes
@@ -27,18 +26,18 @@ class labels_reassigned_dataset(data.Dataset):
     """
 
     def __init__(self, original_dataset, image_indexes, pseudolabels, transform=None):
-        
+
         self.imgs = self.make_dataset(image_indexes, pseudolabels, original_dataset)
         self.transform = transform
-      
+
     def make_dataset(self, image_indexes, pseudolabels, dataset):
 
         images = []
         for idx in image_indexes:
             path = dataset[idx][0]
-            images.append( (path, pseudolabels[idx]) )
+            images.append((path, pseudolabels[idx]))
         return images
-    
+
     def __getitem__(self, index):
         """
         Args:
@@ -55,68 +54,67 @@ class labels_reassigned_dataset(data.Dataset):
     def __len__(self):
         return len(self.imgs)
 
-def clustered_data_indices_to_list(clustered_data_indices):
 
+def clustered_data_indices_to_list(clustered_data_indices):
     pseudolabels = []
     image_indexes = []
-    
+
     for cluster, images in enumerate(clustered_data_indices):
         image_indexes.extend(images)
         pseudolabels.extend([cluster] * len(images))
-        
+
     indexes = np.argsort(image_indexes)
-    
+
     pseudolabels = np.asarray(pseudolabels)[indexes]
-    
+
     return [np.sort(image_indexes), pseudolabels]
 
+
 class neural_features_kmeans_with_preprocessing():
-    
-    
+
     def __init__(self, data, n_clusters, pca=0, verbose=0, **kwargs):
-        
+
         self.data = data
         self.n_clusters = n_clusters
         self.sklearn_kmeans_args = kwargs
-        self.verbose= verbose
-        self.pca=pca
-        
-        self.clustered_data_indices = [ [] for i in range(n_clusters) ]
-        
+        self.verbose = verbose
+        self.pca = pca
+
+        self.clustered_data_indices = [[] for i in range(n_clusters)]
+
         self.preprocessed_data = None
         self.inertia = None
         self.assignments = None
 
     def cluster(self):
-     
+
         end = time.time()
-    
+
         # Preprocess features
-        self.preprocessed_data = self.__preprocess_neural_features(self.data, pca=self.pca)
-        
+        self.preprocessed_data = self.__preprocess_neural_features(data=self.data, pca=self.pca)
+
         if self.verbose:
             print('Preprocessing Features (PCA, Whitening, L2_normalization) Time: {0:.0f} s'.format(time.time() - end))
-    
-        kmeans_object = KMeans(self.n_clusters, max_iter= self.sklearn_kmeans_args.get("max_iter",20),
-                           n_init= self.sklearn_kmeans_args.get("n_init",1) )
-    
+
+        kmeans_object = KMeans(self.n_clusters, max_iter=self.sklearn_kmeans_args.get("max_iter", 20),
+                               n_init=self.sklearn_kmeans_args.get("n_init", 1))
+
         kmeans_object.fit_predict(self.preprocessed_data)
-        
+
         if self.verbose:
             print('k-means time: {0:.0f} s'.format(time.time() - end))
-        
+
         self.inertia = kmeans_object.inertia_
         self.assignments = kmeans_object.labels_
-        
+
         if self.verbose:
-            print('k-means loss evolution (inertia): {0}'.format(self.inertia) )
-        
-        for i in range( len(self.data) ):
-            self.clustered_data_indices[ self.assignments[i] ].append(i)
+            print('k-means loss evolution (inertia): {0}'.format(self.inertia))
 
+        for i in range(len(self.data)):
+            self.clustered_data_indices[self.assignments[i]].append(i)
 
-    def __preprocess_neural_features(data, pca=256):
-        
+    def __preprocess_neural_features(self, data, pca=256):
+
         """Preprocess an array of features.
         Args:
             data (np.array N * ndim): features to preprocess
@@ -125,16 +123,15 @@ class neural_features_kmeans_with_preprocessing():
             np.array of dim N * pca: data PCA-reduced, whitened and L2-normalized
         """
         _, ndim = data.shape
-        data =  data.astype('float32')
-    
+        data = data.astype('float32')
+
         # Apply PCA-whitening with sklearn pca
         mat = PCA(n_components=pca, whiten=True)
         mat.fit(data)
         data = mat.transform(data)
-        
+
         # L2 normalization
         row_sums = np.linalg.norm(data, axis=1)
         data = data / row_sums[:, np.newaxis]
-        
+
         return data
-          
--- models.py
+++ models.py
@@ -12,8 +12,9 @@ import utils
 import numpy as np
 import os
 
+
 class NetBuilder(nn.Module):
-    
+
     def __init__(self, features, classifier, top_layer, apply_sobel):
         super(NetBuilder, self).__init__()
         self.sobel = None
@@ -23,8 +24,8 @@ class NetBuilder(nn.Module):
         self._initialize_weights()
 
         if apply_sobel:
-            self.sobel = make_sobel_layer()             
-            
+            self.sobel = make_sobel_layer()
+
     def forward(self, x):
         if self.sobel:
             x = self.sobel(x)
@@ -34,39 +35,39 @@ class NetBuilder(nn.Module):
         if self.top_layer:
             x = self.top_layer(x)
         return x
-    
+
     def _initialize_weights(self):
         for y, m in enumerate(self.modules()):
-            
+
             if isinstance(m, nn.Conv2d):
-                
+
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                
+
                 for i in range(m.out_channels):
                     m.weight.data[i].normal_(0, math.sqrt(2. / n))
-                    
+
                 if m.bias is not None:
                     m.bias.data.zero_()
-                    
+
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-                
+
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
+
 def make_convolutional_layers(cfg, input_n_channels, bn):
-    
-    # cfg: 
+    # cfg:
     # (number of filters, kernel size, stride, pad)
     # ('M', kernel size, stride)
-    
+
     layers = []
     in_channels = input_n_channels
     for v in cfg:
         if v[0] == 'M':
-            layers += [nn.MaxPool2d(kernel_size= v[1], stride=v[2] ) ]
+            layers += [nn.MaxPool2d(kernel_size=v[1], stride=v[2])]
         else:
             conv2d = nn.Conv2d(in_channels, v[0], kernel_size=v[1],
                                stride=v[2], padding=v[3])
@@ -77,81 +78,82 @@ def make_convolutional_layers(cfg, input_n_channels, bn):
             in_channels = v[0]
     return nn.Sequential(*layers)
 
+
 def make_sobel_layer():
-    
     grayscale = nn.Conv2d(3, 1, kernel_size=1, stride=1, padding=0)
     grayscale.weight.data.fill_(1.0 / 3.0)
     grayscale.bias.data.zero_()
-    
+
     sobel_filter = nn.Conv2d(1, 2, kernel_size=3, stride=1, padding=1)
-    
+
     sobel_filter.weight.data[0, 0].copy_(
         torch.FloatTensor([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     )
     sobel_filter.weight.data[1, 0].copy_(
         torch.FloatTensor([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     )
-    
+
     sobel_filter.bias.data.zero_()
-    
+
     sobel = nn.Sequential(grayscale, sobel_filter)
-    
+
     for p in sobel.parameters():
-        p.requires_grad = False  
-        
+        p.requires_grad = False
+
     return sobel
+
 
 def make_linear_layers(cfg):
     layers = []
     for v in cfg:
-        if v[0]=='L':
-            layers+= [ nn.Linear(v[1],v[2]) ]
-        if v[0]=='R':
-            layers+= [ nn.ReLU( inplace=v[1] ) ]
+        if v[0] == 'L':
+            layers += [nn.Linear(v[1], v[2])]
+        if v[0] == 'R':
+            layers += [nn.ReLU(inplace=v[1])]
             continue
-        if v[0]=='D':
-            layers+= [ nn.Dropout( v[1] ) ]
-            continue    
+        if v[0] == 'D':
+            layers += [nn.Dropout(v[1])]
+            continue
     return nn.Sequential(*layers)
 
+
 def alexnet_cifar(sobel=False, bn=True, out=10):
-    
     input_n_channels = 2 + int(not sobel)
-    
+
     features_cfg = [(64, 3, 1, 2), ('M', 2, None), (192, 3, 1, 2),
                     ('M', 2, None), (384, 3, 1, 1), (256, 3, 1, 1),
                     (256, 3, 1, 1), ('M', 3, 2)]
-    
-    classifier_cfg = [ ('D', 0.5), ('L', 4096, 2048), ('R', True), ('D', 0.5),
-                      ('L', 2048, 2048), ('R', True) ]
-    
+
+    classifier_cfg = [('D', 0.5), ('L', 4096, 2048), ('R', True), ('D', 0.5),
+                      ('L', 2048, 2048), ('R', True)]
+
     top_layer_cfg = [('L', 2048, out)]
-    
+
     features = make_convolutional_layers(features_cfg, input_n_channels, bn=bn)
     classifier = make_linear_layers(classifier_cfg)
     top_layer = make_linear_layers(top_layer_cfg)
-    
+
     model = NetBuilder(features, classifier, top_layer, sobel)
-    
+
     return model
 
+
 def lenet_5(bn=False, out=10):
-    
-    features_cfg = [(6,5,1,0), 'M', (16, 5, 1, 0), 'M']
-    
+    features_cfg = [(6, 5, 1, 0), 'M', (16, 5, 1, 0), 'M']
+
     classifier_cfg = [('L', 400, 120), ('R', True),
                       ('L', 120, 84), ('R', True)]
-    
+
     top_layer_cfg = [('L', 84, out)]
-    
+
     features = make_convolutional_layers(features_cfg, 1, bn=bn)
     classifier = make_linear_layers(classifier_cfg)
     top_layer = make_linear_layers(top_layer_cfg)
-    
+
     model = NetBuilder(features, classifier, top_layer, False)
-    
-    return model    
- 
+
+    return model
+
 def remove_top_layer(model):
     """
     :param model: The model than need its top layer to be removed
@@ -161,35 +163,34 @@ def remove_top_layer(model):
     model.top_layer = None
     model.classifier = nn.Sequential(*list(model.classifier.children())[:-1])
     return new_output_size
-    
+
 def add_top_layer(model, top_layer_cfg, device):
-    
     mlp = list(model.classifier.children())
     mlp.append(nn.ReLU(inplace=True).to(device))
     model.classifier = nn.Sequential(*mlp)
-    
+
     model.top_layer = make_linear_layers(top_layer_cfg)
     model.top_layer.weight.data.normal_(0, 0.01)
     model.top_layer.bias.data.zero_()
-    
+
     model.top_layer.to(device)
-    
+
+
 def compute_network_output(dataloader, model, device, batch_size, data_size, verbose=False,
                            return_inputs=False, return_targets=False):
-    
     if verbose:
         print('Computing Model Output')
-        
+
     batch_time = utils.AverageMeter()
     end = time.time()
-    
+
     model.eval()
-    
+
     inputs = []
     targets = []
 
     for i, (input_tensor, target) in enumerate(dataloader):
-        
+
         input = input_tensor.to(device)
         output = model(input).data.cpu().numpy()
 
@@ -198,7 +199,7 @@ def compute_network_output(dataloader, model, device, batch_size, data_size, ver
 
         if i < len(dataloader) - 1:
             outputs[i * batch_size: (i + 1) * batch_size] = output.astype('float32')
-            
+
         else:
             # special treatment for final batch
             outputs[i * batch_size:] = output.astype('float32')
@@ -206,32 +207,33 @@ def compute_network_output(dataloader, model, device, batch_size, data_size, ver
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
-        
-        inputs.extend( input_tensor.data.numpy() )
-        targets.extend( target.data.numpy() )
-        
+
+        inputs.extend(input_tensor.data.numpy())
+        targets.extend(target.data.numpy())
+
         if verbose and (i % 10) == 0:
             print('{0} / {1}\t'
                   'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})'
                   .format(i, len(dataloader), batch_time=batch_time))
-    
+
     inputs = np.squeeze(np.array(inputs))
     targets = np.array(targets)
-    
-       
-    if(return_inputs and return_targets):
-      return inputs, outputs, targets
-  
-    if(not return_inputs and not return_targets):
-      return outputs
-  
-    if(return_inputs):
-      return inputs, outputs
-  
-    if(return_targets):
-      return outputs, targets     
 
-def deep_cluster_train(dataloader, model, loss_criterion, net_optimizer, annxed_layers_optimizer, epoch, device, return_inputs_outputs_targets_losses=False, verbose=True, checkpoint=0):
+    if (return_inputs and return_targets):
+        return inputs, outputs, targets
+
+    if (not return_inputs and not return_targets):
+        return outputs
+
+    if (return_inputs):
+        return inputs, outputs
+
+    if (return_targets):
+        return outputs, targets
+
+
+def deep_cluster_train(dataloader, model, loss_criterion, net_optimizer, annxed_layers_optimizer, epoch, device,
+                       return_inputs_outputs_targets_losses=False, verbose=True, checkpoint=0):
     """Training of the CNN.
         Args:
             loader (torch.utils.data.DataLoader): Data loader
@@ -244,37 +246,35 @@ def deep_cluster_train(dataloader, model, loss_criterion, net_optimizer, annxed_
     batch_time = utils.AverageMeter()
     losses = utils.AverageMeter()
     data_time = utils.AverageMeter()
-    
+
     # switch to train mode
     model.train()
 
     end = time.time()
-    
-    
+
     dataset_inputs = []
     dataset_outputs = []
     dataset_targets = []
     dataset_losses = []
-    
 
     for i, (input_tensor, target_tensor) in enumerate(dataloader):
-        
+
         data_time.update(time.time() - end)
 
         target = target_tensor.to(device)
         input = input_tensor.to(device)
 
         output = model(input)
-        
+
         loss = loss_criterion(output, target)
-        if(loss.dim()==0):
-            print("Error This function excpects a loss criterion that doesn't apply reduction\n")        
-        
-        dataset_inputs.extend( input_tensor.data.numpy() )
-        dataset_losses.extend( loss.data.cpu().numpy() )
-        dataset_targets.extend( target_tensor.data.numpy() )
-        dataset_outputs.extend( output.cpu().data.numpy() )
-        
+        if (loss.dim() == 0):
+            print("Error This function excpects a loss criterion that doesn't apply reduction\n")
+
+        dataset_inputs.extend(input_tensor.data.numpy())
+        dataset_losses.extend(loss.data.cpu().numpy())
+        dataset_targets.extend(target_tensor.data.numpy())
+        dataset_outputs.extend(output.cpu().data.numpy())
+
         loss = loss.mean()
 
         # record loss
@@ -285,7 +285,7 @@ def deep_cluster_train(dataloader, model, loss_criterion, net_optimizer, annxed_
         loss.backward()
         net_optimizer.step()
         annxed_layers_optimizer.step()
-        
+
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
@@ -297,51 +297,48 @@ def deep_cluster_train(dataloader, model, loss_criterion, net_optimizer, annxed_
                   'Loss: {loss.val:.4f} ({loss.avg:.4f})'
                   .format(epoch, i, len(dataloader), batch_time=batch_time,
                           data_time=data_time, loss=losses))
-            
-    if(return_inputs_outputs_targets_losses):
-      return losses.avg, dataset_inputs, dataset_outputs, dataset_targets, dataset_losses
+
+    if (return_inputs_outputs_targets_losses):
+        return losses.avg, dataset_inputs, dataset_outputs, dataset_targets, dataset_losses
     else:
-      return losses.avg
-  
+        return losses.avg
+
+
 def deep_cluster_test(dataloader, model, device):
-    
-    correct_predictions=0
-    total_predictions=0
+    correct_predictions = 0
+    total_predictions = 0
     model.eval()
-  
+
     for input_tensor, target in dataloader:
-      
-      target = target.to(device)
-      input_tensor = input_tensor.to(device)
-      
-      output = model(input_tensor)
-      
-      predicted = torch.argmax(output,1)
-      
-      total_predictions+=target.size(0)
-      
-      correct_predictions+= (predicted==target).sum().item()
-    
-    test_acc=(100*correct_predictions/total_predictions)  
-    
-    return test_acc        
+        target = target.to(device)
+        input_tensor = input_tensor.to(device)
+
+        output = model(input_tensor)
+
+        predicted = torch.argmax(output, 1)
+
+        total_predictions += target.size(0)
+
+        correct_predictions += (predicted == target).sum().item()
+
+    test_acc = (100 * correct_predictions / total_predictions)
+
+    return test_acc
+
 
 def save_checkpoint(model, optimizer, epoch, checkpoint, path, architecture="unspecified", verbose=True):
-    
     path = os.path.join(
         path,
         'checkpoints',
         'checkpoint_' + checkpoint + '.pth.tar',
     )
-    
+
     if verbose:
         print('Save checkpoint at: {0}'.format(path))
-        
+
     torch.save({
         'epoch': epoch + 1,
         'arch': architecture,
         'state_dict': model.state_dict(),
-        'optimizer' : optimizer.state_dict()
+        'optimizer': optimizer.state_dict()
     }, path)
-
-    
