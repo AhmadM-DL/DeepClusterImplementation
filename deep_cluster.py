fb99db46608df7b2bcdfb3b8cfc37d1aadb846f6
amm90 <amm90@mail.aub.edu>
2019-12-04 21:04:56 +0200

    Quick

--- deep_cluster.py
+++ deep_cluster.py
@@ -257,7 +257,7 @@ class ClusteringTracker(object):
              # The file dosen't exist
             print("The provided path %s doesn't exist" % path)
         else:
-            self.clustering_log=np.load(path, allow_pickle=True)
+            self.clustering_log = np.load(path, allow_pickle=True).tolist()
             print("Loaded Clustering Log from : %s" % path)
 
     def plot_cluster_evolution(self, cluster_evolution, final_epoch, weight_in_percent=True):
