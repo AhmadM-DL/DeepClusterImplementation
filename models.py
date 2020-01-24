06abc9d419a0f26c90afc645f104d29b8b9154ec
AhmadM-DL <amm90@mail.aub.edu>
2020-01-24 19:09:30 +0200

    Quick

--- models.py
+++ models.py
@@ -545,7 +545,7 @@ def add_probe_layer(model, layer_input_size, layer_output_size, device, weight_m
 
     layers.append(probe_layer)
 
-    return nn.sequential(*layers)
+    return nn.Sequential(*layers)
 
 def save_model_parameter(model, path, override=False):
     if not os.path.isfile(path):
