1548cd275f326cefcb88e6306fb10456f695d606
AhmadM-DL <amm90@mail.aub.edu>
2020-05-12 09:36:00 +0300

    Multiple datasets types support in Deep clustering dataset

--- deep_clustering_dataset.py
+++ deep_clustering_dataset.py
@@ -5,6 +5,7 @@ Created on Tuesday April 20 2020
 
 from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
+from torchvision.datasets import VisionDataset
 import torch
 import numpy as np
 
@@ -20,7 +21,16 @@ class DeepClusteringDataset(Dataset):
 
     def __init__(self, original_dataset):
         self.original_dataset = original_dataset
-        self.imgs = self.original_dataset.imgs.copy()
+
+        if isinstance(self.original_dataset, ImageFolder):
+            self.imgs = self.original_dataset.imgs.copy()
+
+        elif isinstance(self.original_dataset_type, VisionDataset):
+            self.data = original_dataset.data
+            self.targets = original_dataset.target
+        else:
+            raise Exception("The passed original dataset is of unsupported dataset instance")
+
         self.transform = original_dataset.transform
             
     def __len__(self):
@@ -30,21 +40,64 @@ class DeepClusteringDataset(Dataset):
         return self.original_dataset.__getitem__(index)
 
     def get_targets(self):
-        return [target for (path ,target) in self.original_dataset.imgs]
+        if isinstance(self.original_dataset, ImageFolder):
+            return [target for (path ,target) in self.original_dataset.imgs]
+
+        elif isinstance(self.original_dataset_type, VisionDataset):
+            return self.targets
+
+        else:
+            raise Exception("The passed original dataset is of unsupported dataset instance")
     
     def set_pseudolabels(self, pseudolabels):
-        for i, pseudolabel in enumerate(pseudolabels):
-            self.imgs[i] = (self.imgs[i][0], torch.tensor(pseudolabel, dtype=torch.long))
+
+        if isinstance(self.original_dataset, ImageFolder):
+            for i, pseudolabel in enumerate(pseudolabels):
+                self.imgs[i] = (self.imgs[i][0], torch.tensor(pseudolabel, dtype=torch.long))
+
+        elif isinstance(self.original_dataset_type, VisionDataset):
+             self.targets = pseudolabels
+
+        else:
+            raise Exception("The passed original dataset is of unsupported dataset instance")
     
     def get_pseudolabels(self):
-        return [pseudolabel.item() for (path, pseudolabel) in self.imgs]
-    
+        if isinstance(self.original_dataset, ImageFolder):
+            return [pseudolabel.item() for (path, pseudolabel) in self.imgs]
+
+        elif isinstance(self.original_dataset_type, VisionDataset):
+             return self.targets
+
+        else:
+            raise Exception("The passed original dataset is of unsupported dataset instance")
+        
     def unset_pseudolabels(self):
-        self.imgs= self.original_dataset.imgs
+        if isinstance(self.original_dataset, ImageFolder):
+            self.imgs= self.original_dataset.imgs
+
+        elif isinstance(self.original_dataset_type, VisionDataset):
+             self.targets = self.original_dataset.targets
+
+        else:
+            raise Exception("The passed original dataset is of unsupported dataset instance")
+                
 
     def group_indices_by_labels(self):
-        n_labels = len(np.unique([ label for (_, label) in self.imgs]))
-        grouped_indices = [[] for i in range(n_labels)]
-        for i, (path, label) in enumerate(self.imgs):
-            grouped_indices[label].append(i)
-        return grouped_indices
+        if isinstance(self.original_dataset, ImageFolder):
+            n_labels = len(np.unique([ label for (_, label) in self.imgs]))
+            grouped_indices = [[] for i in range(n_labels)]
+            for i, (path, label) in enumerate(self.imgs):
+                grouped_indices[label].append(i)
+            return grouped_indices
+
+        elif isinstance(self.original_dataset_type, VisionDataset):
+            n_labels = len(np.unique(self.targets))
+            grouped_indices = [[] for i in range(n_labels)]
+            for i, label in enumerate(self.targets):
+                grouped_indices[label].append(i)
+            return grouped_indices
+            
+        else:
+            raise Exception("The passed original dataset is of unsupported dataset instance")
+             
+
