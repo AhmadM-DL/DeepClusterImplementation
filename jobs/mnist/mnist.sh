f8d7789b2d76eaa65d04ecdb86e2216410639261
AhmadM-DL <amm90@mail.aub.edu>
2020-07-31 22:04:42 +0300

    job

--- jobs/mnist/mnist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LRs=seq 0.1 2 10
-WD=0.00001
-K=10
-PYTHON="C:\ProgramData\Anaconda3\python.exe"
-
-${PYTHON} main.py ${DIR} --exp ${EXP} --arch ${ARCH} \
-  --lr ${LR} --wd ${WD} --k ${K} --sobel --verbose --workers ${WORKERS}
