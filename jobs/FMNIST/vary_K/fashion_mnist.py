e06b6c1a582143441b71d767aeecf94025f5e7eb
AhmadM-DL <amm90@mail.aub.edu>
2021-01-15 11:59:02 +0200

    added fashion mnist vary_k and cifar 100 baseline

--- jobs/FMNIST/vary_K/fashion_mnist.py
+++ jobs/FMNIST/vary_K/fashion_mnist.py
@@ -2,39 +2,39 @@ import torch
 import json
 import logging
 import argparse
-from datetime import datetime
 
 import sys
 import importlib
-import os
 
 sys.path.append("C:\\Users\\PC\\Desktop\\Projects\\DeepClusterImplementation")
 
+
 from torchvision import transforms
-from torchvision.datasets import CIFAR100
+from torchvision.datasets import FashionMNIST
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import Normalize, ToTensor, Resize, CenterCrop
-from deep_clustering_models import AlexNet_Micro
+from deep_clustering_models import LeNet
 from deep_clustering_dataset import DeepClusteringDataset
 from deep_clustering import deep_cluster
 
 from evaluation.linear_probe import eval_linear
 from utils import set_seed
 
+
 def run(device, batch_norm, lr, wd, momentum, n_cycles,
-        n_clusters, pca, training_batch_size, sobel, training_shuffle,
+        n_clusters, pca, training_batch_size, training_shuffle,
         random_state, dataset_path):
 
     logging.info("Set Seed")
     set_seed(random_state)
     
     logging.info("Loading Dataset")
-    cifar100 = CIFAR100(dataset_path, download=True)
+    fashion_mnist = FashionMNIST(dataset_path, download=True)
 
     device = torch.device(device)
 
     logging.info("Build Model")
-    model = AlexNet_Micro(sobel=sobel, batch_normalization=batch_norm, device=device)
+    model = LeNet(batch_normalization=batch_norm, device=device)
 
     logging.info("Build Optimizer")
     optimizer = torch.optim.SGD(
@@ -48,11 +48,11 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
     loss_function = torch.nn.CrossEntropyLoss()
 
     logging.info("Decorating Dataset")
-    dataset = DeepClusteringDataset(cifar100)
+    dataset = DeepClusteringDataset(fashion_mnist)
 
     logging.info("Defining Transformations")
-    normalize = transforms.Normalize(mean = (0.437, 0.443, 0.472),
-                                     std = (0.198, 0.201, 0.197))
+    normalize = transforms.Normalize(mean=(0.2860,),
+                                     std=(0.3205,))
 
     training_transform = transforms.Compose([
         transforms.RandomResizedCrop(32),
@@ -60,13 +60,13 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
         normalize])
 
     loading_transform = transforms.Compose([
-        transforms.Resize(42),
+        transforms.Resize(44),
         transforms.CenterCrop(32),
         transforms.ToTensor(),
         normalize])
 
     logging.info("Defining Writer")
-    writer_file = "cifar100_microAlexNet_"+"batchnorm(%d)_" % batch_norm + \
+    writer_file = "fashion_mnist_"+"batchnorm(%d)_" % batch_norm + \
         "lr(%f)_" % lr+"momentum(%f)_" % momentum+"wdecay(%f)_" % wd + \
         "n_clusters(%d)_" % n_clusters + \
         "n_cycles(%d)_" % n_cycles+"rnd(%d)_" % random_state + \
@@ -77,15 +77,9 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
     else:
         writer_file=writer_file+"pca(None)_"
 
-    writer = SummaryWriter('runs/'+writer_file)
+    writer = SummaryWriter(
+        'runs/'+writer_file)
 
-    if os.path.isfile("checkpoints/"+writer_file+"/last_model.pth"):
-        resume = "checkpoints/"+writer_file+"/last_model.pth"
-        logging.info("\n##########\nResuming from: %s\n##########"%resume)
-    else: 
-        resume = None
-        logging.info("\n##########\nRun: %s\n##########"%writer_file)
-    
     deep_cluster(model=model,
                  dataset=dataset,
                  n_clusters=n_clusters,
@@ -100,37 +94,35 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
                  training_shuffle=training_shuffle,
                  pca_components=pca,
                  checkpoints= "checkpoints/"+writer_file,
-                 resume=resume,
                  writer=writer)
     
-    cifar100_test = CIFAR100(dataset_path, train=False, download=True)
+    fashion_mnist_test = FashionMNIST(dataset_path, train=False, download=True)
 
-    traindataset = cifar100
-    validdataset = cifar100_test
+    traindataset = fashion_mnist
+    validdataset = fashion_mnist_test
 
-    transformations_val = [transforms.Resize(42),
+    transformations_val = [transforms.Resize(44),
                                 transforms.CenterCrop(32),
                                 transforms.ToTensor(),
                                 normalize]
 
-    transformations_train = [transforms.Resize(42),
+    transformations_train = [transforms.Resize(44),
                                 transforms.CenterCrop(32),
                                 #transforms.RandomCrop(32),
-                                #transforms.RandomHorizontalFlip(),
+                                transforms.RandomHorizontalFlip(),
                                 transforms.ToTensor(),
                                 normalize]
 
-    cifar100.transform = transforms.Compose(transformations_train)
-    cifar100_test.transform = transforms.Compose(transformations_val)
+    fashion_mnist.transform = transforms.Compose(transformations_train)
+    fashion_mnist_test.transform = transforms.Compose(transformations_val)
 
-    logging.info("Evaluation")
     eval_linear(model=model,
                 n_epochs=20,
                 traindataset=traindataset,
                 validdataset=validdataset,
-                target_layer="relu_5",
-                n_labels=100,
-                features_size= 512,
+                target_layer="conv_2",
+                n_labels=10,
+                features_size=1600,
                 avg_pool= None,
                 random_state=random_state,
                 writer= writer,
@@ -139,27 +131,17 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='PyTorch Implementation of DeepCluster')
-    parser.add_argument('--hyperparam', default="./hyper.json", type=str, help='Path to hyperparam json file')
-    parser.add_argument('--dataset', default="../datasets", type=str, help="Path to datasets")
+    parser.add_argument('--hyperparam', default="jobs/mnist/hyper.json", type=str, help='Path to hyperparam json file')
+    parser.add_argument('--dataset', default="./datasets", type=str, help="Path to datasets")
     parser.add_argument('--device', default="cpu", type=str, help="Device to use")
-    parser.add_argument('--seed', default=666, type=int, help="Random Seed")
-    parser.add_argument('--chkp', default=None, type=str, help="Checkpoint")
+    parser.add_argument("--seed", default=666, type=int, help="Random Seed")
     args = parser.parse_args()
 
-    logging.basicConfig(filename='app.log', filemode='a',
-                        format='%(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-
-    logging.info("\n##########\n%s\n##########\n"%datetime.now())
-
+    logging.basicConfig(filename='app.log', filemode='w',
+                        format='%(name)s - %(levelname)s - %(message)s')
     hparams = json.load(open(args.hyperparam, "r"))
     device = torch.device(args.device)
-    if args.chkp:
-        executed_runs = int(open(args.chkp, "r").read())
-        logging.info("Running from checkpoint: run(%d)"%executed_runs)
-    else:
-        executed_runs = 0 
-    counter=1
+
     for lr in hparams['lr']:
         for wd in hparams['wd']:
             for momentum in hparams['momentum']:
@@ -169,10 +151,6 @@ if __name__ == '__main__':
                             for pca in hparams["pca"]:
                                 for training_batch_size in hparams["training_batch_size"]:
                                     for training_shuffle in hparams["training_shuffle"]:
-                                        for sobel in hparams["sobel"]:
-                                            if counter <= executed_runs:
-                                                continue
+                                        for seed in hparams["seed"]:
                                             run(device, batch_norm, lr, wd, momentum, n_cycles, n_clusters,
-                                            pca, training_batch_size, training_shuffle, sobel, random_state=args.seed, dataset_path=args.dataset)
-                                            counter+=1
-                                            open(args.chpk, "w").write(str(counter))
+                                            pca, training_batch_size, training_shuffle, random_state=seed, dataset_path=args.dataset)
