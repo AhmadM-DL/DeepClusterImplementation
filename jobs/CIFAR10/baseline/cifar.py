0a1bb8ba714e0879569a25d9294ee4f7ed580837
AhmadM-DL <amm90@mail.aub.edu>
2021-02-28 14:32:58 +0200

    cleaned!

--- jobs/CIFAR10/baseline/cifar.py
+++ jobs/CIFAR10/baseline/cifar.py
@@ -1,3 +1,12 @@
+from utils import set_seed
+from evaluation.linear_probe import eval_linear
+from deep_clustering import deep_cluster
+from deep_clustering_dataset import DeepClusteringDataset
+from deep_clustering_models import AlexNet_Small
+from torchvision.transforms import Normalize, ToTensor, Resize, CenterCrop
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
 import torch
 import json
 import logging
@@ -11,29 +20,20 @@ import os
 
 sys.path.append("C:\\Users\\PC\\Desktop\\Projects\\DeepClusterImplementation")
 
-from torchvision import transforms
-from torchvision.datasets import CIFAR10
-from torch.utils.tensorboard import SummaryWriter
-from torchvision.transforms import Normalize, ToTensor, Resize, CenterCrop
-from deep_clustering_models import AlexNet_Small
-from deep_clustering_dataset import DeepClusteringDataset
-from deep_clustering import deep_cluster
-
-from evaluation.linear_probe import eval_linear
-from utils import set_seed
 
 DATASET = "CIFAR10"
 MODEL = "AlexNetSmall"
 CHECKPOINTS = "checkpoints"
 TENSORBOARD = "runs"
 EXPERIMENT_CHECK = "exp.chkp"
-LOGS= "exp.log"
+LOGS = "exp.log"
+
 
 def run(device, batch_norm, lr, wd, momentum, n_cycles,
         n_clusters, pca, training_batch_size, sobel, training_shuffle,
         random_state, dataset_path, use_faiss, log_dir=None):
     logging.info("New Experiment ##########################################")
-    logging.info("%s"%datetime.now())
+    logging.info("%s" % datetime.now())
 
     logging.info("Set Seed")
     set_seed(random_state)
@@ -41,14 +41,15 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
     logging.info("Set Log Dir")
     if not log_dir:
         log_dir = "./"
-    
+
     logging.info("Loading Dataset")
     cifar10 = CIFAR10(dataset_path, download=True, train=True)
 
     device = torch.device(device)
 
     logging.info("Build Model")
-    model = AlexNet_Small(sobel=sobel, batch_normalization=batch_norm, device=device)
+    model = AlexNet_Small(
+        sobel=sobel, batch_normalization=batch_norm, device=device)
 
     logging.info("Build Optimizer")
     optimizer = torch.optim.SGD(
@@ -65,8 +66,8 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
     dataset = DeepClusteringDataset(cifar10)
 
     logging.info("Defining Transformations")
-    normalize = transforms.Normalize(mean = (0.437, 0.443, 0.472),
-                                     std = (0.198, 0.201, 0.197))
+    normalize = transforms.Normalize(mean=(0.437, 0.443, 0.472),
+                                     std=(0.198, 0.201, 0.197))
 
     training_transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -83,23 +84,24 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
     writer_file = "{dataset}_{model}_batchnorm({bt})_lr({lr})_momentum({mom})_wdecay({wd})_n_clusters({nclusters})_\
                    n_cycles(ncycles)_rnd(seed)_t_batch_size(tbsize)_shuffle(shfl)_sobel(sobel)_"
     writer_file = writer_file.format(dataset=DATASET, model=MODEL, bt=batch_norm, lr=lr, mom=momentum,
-                                     wd=wd, nclusters= n_clusters, ncycles=n_cycles, seed=random_state,
+                                     wd=wd, nclusters=n_clusters, ncycles=n_cycles, seed=random_state,
                                      tbsize=training_batch_size, sobel=sobel
                                      )
     if pca:
-        writer_file=writer_file+"pca(%d)"%pca
+        writer_file = writer_file+"pca(%d)" % pca
     else:
-        writer_file=writer_file+"pca(None)"
+        writer_file = writer_file+"pca(None)"
 
-    writer = SummaryWriter( os.path.join(log_dir, TENSORBOARD, writer_file) )
+    writer = SummaryWriter(os.path.join(log_dir, TENSORBOARD, writer_file))
 
-    if os.path.isfile( os.path.join(log_dir, CHECKPOINTS, writer_file, "last_model.pth") ):
-        resume = os.path.join(log_dir, CHECKPOINTS, writer_file, "last_model.pth")
-        logging.info("Resuming from: %s"%resume)
-    else: 
+    if os.path.isfile(os.path.join(log_dir, CHECKPOINTS, writer_file, "last_model.pth")):
+        resume = os.path.join(log_dir, CHECKPOINTS,
+                              writer_file, "last_model.pth")
+        logging.info("Resuming from: %s" % resume)
+    else:
         resume = None
-        logging.info("Run: %s"%writer_file)
-    
+        logging.info("Run: %s" % writer_file)
+
     deep_cluster(model=model,
                  dataset=dataset,
                  n_clusters=n_clusters,
@@ -113,27 +115,27 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
                  training_batch_size=training_batch_size,
                  training_shuffle=training_shuffle,
                  pca_components=pca,
-                 checkpoints= os.path.join(log_dir, CHECKPOINTS, writer_file),
-                 use_faiss= use_faiss,
+                 checkpoints=os.path.join(log_dir, CHECKPOINTS, writer_file),
+                 use_faiss=use_faiss,
                  resume=resume,
                  writer=writer)
-    
+
     cifar10_test = CIFAR10(dataset_path, train=False, download=True)
 
     traindataset = cifar10
     validdataset = cifar10_test
 
     transformations_val = [transforms.Resize(256),
-                                transforms.CenterCrop(224),
-                                transforms.ToTensor(),
-                                normalize]
+                           transforms.CenterCrop(224),
+                           transforms.ToTensor(),
+                           normalize]
 
     transformations_train = [transforms.Resize(256),
-                                transforms.CenterCrop(224),
-                                #transforms.RandomCrop(32),
-                                #transforms.RandomHorizontalFlip(),
-                                transforms.ToTensor(),
-                                normalize]
+                             transforms.CenterCrop(224),
+                             # transforms.RandomCrop(32),
+                             # transforms.RandomHorizontalFlip(),
+                             transforms.ToTensor(),
+                             normalize]
 
     cifar10.transform = transforms.Compose(transformations_train)
     cifar10_test.transform = transforms.Compose(transformations_val)
@@ -145,40 +147,52 @@ def run(device, batch_norm, lr, wd, momentum, n_cycles,
                 validdataset=validdataset,
                 target_layer="relu_5",
                 n_labels=10,
-                features_size= 9216,
-                avg_pool= {"kernel_size":2, "stride":2, "padding":0},
+                features_size=9216,
+                avg_pool={"kernel_size": 2, "stride": 2, "padding": 0},
                 random_state=random_state,
-                writer= writer,
+                writer=writer,
                 verbose=1)
+
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='PyTorch Implementation of DeepCluster')
-    parser.add_argument('--hyperparam', default="./hyper.json", type=str, help='Path to hyperparam json file')
-    parser.add_argument('--dataset', default="../datasets", type=str, help="Path to datasets")
-    parser.add_argument('--device', default="cpu", type=str, help="Device to use")
+    parser = argparse.ArgumentParser(
+        description='PyTorch Implementation of DeepCluster')
+    parser.add_argument('--hyperparam', default="./hyper.json",
+                        type=str, help='Path to hyperparam json file')
+    parser.add_argument('--dataset', default="../datasets",
+                        type=str, help="Path to datasets")
+    parser.add_argument('--device', default="cpu",
+                        type=str, help="Device to use")
     parser.add_argument('--seed', default=666, type=int, help="Random Seed")
-    parser.add_argument('--log_dir', default=None, type=str, help="Logs directory")
-    parser.add_argument("--use_faiss", action="store_true", help="Use facebook FAISS for clustering")
+    parser.add_argument('--log_dir', default=None,
+                        type=str, help="Logs directory")
+    parser.add_argument("--use_faiss", action="store_true",
+                        help="Use facebook FAISS for clustering")
     args = parser.parse_args()
 
-    logging.basicConfig(filename=  os.path.join(args.log_dir, LOGS), filemode='a',
-                        format='%(message)s',
-                        level=logging.INFO)
+    logging.basicConfig(format='%(message)s',
+                        level=logging.INFO,
+                        handlers=[
+                            logging.FileHandler(filename=os.path.join(args.log_dir, LOGS), mode="a"),
+                            logging.StreamHandler(sys.stdout)
+                        ]
+    )
 
     logging.info("New Batch ####################################")
-    logging.info("\n##########\n%s\n##########\n"%datetime.now())
+    logging.info("\n##########\n%s\n##########\n" % datetime.now())
 
     hparams = json.load(open(args.hyperparam, "r"))
     device = torch.device(args.device)
 
-    if os.path.isfile( os.path.join(args.log_dir, EXPERIMENT_CHECK) ):
-        executed_runs = int(open( os.path.join(args.log_dir, EXPERIMENT_CHECK) , "r").read())
-        logging.info("Running from checkpoint: run(%d)"%executed_runs)
+    if os.path.isfile(os.path.join(args.log_dir, EXPERIMENT_CHECK)):
+        executed_runs = int(
+            open(os.path.join(args.log_dir, EXPERIMENT_CHECK), "r").read())
+        logging.info("Running from checkpoint: run(%d)" % executed_runs)
     else:
-        executed_runs = 0 
+        executed_runs = 0
 
-    counter=1
+    counter = 1
     for lr in hparams['lr']:
         for wd in hparams['wd']:
             for momentum in hparams['momentum']:
@@ -191,19 +205,22 @@ if __name__ == '__main__':
                                         for sobel in hparams["sobel"]:
                                             #logging.info("Experiment %d"%counter)
                                             if counter <= executed_runs:
-                                                counter+=1
+                                                counter += 1
                                                 continue
                                             try:
                                                 run(device, batch_norm, lr, wd, momentum, n_cycles, n_clusters,
-                                                pca, training_batch_size, training_shuffle, sobel,
-                                                random_state=args.seed, dataset_path=args.dataset,
-                                                use_faiss=args.use_faiss, log_dir= args.log_dir)
+                                                    pca, training_batch_size, training_shuffle, sobel,
+                                                    random_state=args.seed, dataset_path=args.dataset,
+                                                    use_faiss=args.use_faiss, log_dir=args.log_dir)
 
-                                                counter+=1
-                                                open( os.path.join(args.log_dir, EXPERIMENT_CHECK), "w").write(str(counter))
+                                                counter += 1
+                                                open(os.path.join(args.log_dir, EXPERIMENT_CHECK), "w").write(
+                                                    str(counter))
                                             except Exception as e:
-                                                logging.error(traceback.format_exception(*sys.exc_info()))
+                                                logging.error(
+                                                    traceback.format_exception(*sys.exc_info()))
                                                 logging.error(e)
-                                                counter+=1
-                                                open( os.path.join(args.log_dir, EXPERIMENT_CHECK), "w").write(str(counter))
+                                                counter += 1
+                                                open(os.path.join(args.log_dir, EXPERIMENT_CHECK), "w").write(
+                                                    str(counter))
                                                 continue
